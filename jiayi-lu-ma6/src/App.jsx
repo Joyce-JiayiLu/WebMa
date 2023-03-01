ecaae672fdb1920e68d9251342e0fef6ed5d3007
jiayilu <lu.jiayi2@northeastern.edu>
2023-02-28 18:40:19 -0800

    finished

--- jiayi-lu-ma6/src/App.jsx
+++ jiayi-lu-ma6/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputColor, setInputColor] = useState('');
+  const [color, setColor] = useState('white');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  function changeColor() {
+    const validColors = ['green', 'blue', 'red'];
+    if (validColors.includes(inputColor)) {
+      setColor(inputColor);
+      setErrorMessage('');
+    } else {
+      setColor('white');
+      setErrorMessage(
+        `${inputColor} is not a valid color. Please choose green, blue or red.`
+      );
+    }
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div id="color-square" style={{ backgroundColor: color }}></div>
+      <input
+        type="text"
+        id="color-input"
+        value={inputColor}
+        onChange={(event) => setInputColor(event.target.value)}
+      />
+      <button id="change-color-button" onClick={changeColor}>
+        Change color
+      </button>
+      <p id="error-message">{errorMessage}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
